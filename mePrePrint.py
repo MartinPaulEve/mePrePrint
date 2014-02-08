8c5b38d6eca5f0da490fcc75c9e258b43afebc64
Martin Eve <martin@martineve.com>
2014-02-08 18:39:11 +0000

    Initial replace functionality

--- mePrePrint.py
+++ mePrePrint.py
@@ -1,5 +1,3 @@
-__author__ = 'martin'
-
 #!/usr/bin/env python
 """mePrePrint: generate a combined PDF document from input files; to be used for green archiving
 
@@ -34,7 +32,12 @@ class MePrePrint (Debuggable):
 
         # initialize debugger
         self.debug = Debug()
+        self.debug.enable_debug()
         Debuggable.__init__(self, 'mePrePrint')
+
+        # get arguments
+        self.doc_type = self.args['--type']
+        self.title = self.args['--article_title']
 
     @staticmethod
     def copy(src, dst):
@@ -45,13 +48,27 @@ class MePrePrint (Debuggable):
                 shutil.copy(src, dst)
             else: raise
 
+    @staticmethod
+    def do_replace(in_string, replace_text, substitute):
+        return in_string.replace(replace_text, substitute)
+
     def create_coversheet(self):
-        tempfile.mkdtemp()
+        # copy the coversheet to a temporary directory
+        destination = tempfile.mkdtemp()
+        self.debug.print_debug(self, u'Copying coversheet to {0}'.format(destination))
         my_dir = os.path.dirname(os.path.realpath(__file__))
+        self.copy(os.path.join(my_dir, u'resources/extracted'), os.path.join(destination, u'coversheet'))
 
-        self.copy(os.path.join(my_dir, 'resources/extracted'))
+        # open the document XML
+        with open (u'{0}'.format(os.path.join(destination, u'coversheet/word/document.xml')), 'r') as doc_file:
+            contents = doc_file.read()
+
+            contents = self.do_replace(contents, '{ARTICLE_TITLE}', self.title)
 
 
+        # remove the temporary file
+        self.debug.print_debug(self, u'Removing temporary directory {0}'.format(destination))
+        shutil.rmtree(destination)
 
     def run(self):
         self.create_coversheet()
